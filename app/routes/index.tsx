985122573a0d9149feacdc131c53455c13631062
Anderson Sousa <and3rsonsousa@outlook.com>
2022-06-27 13:41:28 -0300

    init

---
+++ app/routes/index.tsx
@@ -0,0 +1,12 @@
+export default function Index() {
+	return (
+		<div style={{ display: "grid", placeItems: "center", height: "100vh" }}>
+			<div style={{ textAlign: "center" }}>
+				<img src="/logo.svg" alt="" style={{ width: 200 }} />
+				<p style={{ fontFamily: "sans-serif", color: "#789" }}>
+					Em breve nosso site estará disponível
+				</p>
+			</div>
+		</div>
+	);
+}
